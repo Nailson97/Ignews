116039dd59fea9b2fa47b8ad4d7be044172cca78
Nailson97 <nailsonsousa97@gmail.com>
2022-10-03 14:01:46 -0300

    feat: structure of the finalized application

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Testando Ignews</title>
+      </Head>
+      <h1>Hello Brazil</h1>
+    </>
+  );
+}
