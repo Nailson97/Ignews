f4ff37224d032a3e146939baf764514b9ad7303e
Nailson97 <nailsonsousa97@gmail.com>
2022-10-07 09:58:23 -0300

    feat: finalizing the home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Testando Ignews</title>
+        <title>Home | Ignews</title>
       </Head>
-      <h1>Hello Brazil</h1>
+      <main className={styles.contantContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publication <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
